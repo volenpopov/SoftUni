56f6bf791c8dea39807b924f367dbc88811046b9
Volen Popov <volenpopov@abv.bg>
2018-11-14 17:14:26 +0200

    Add files via upload

---
+++ CSharp Advanced/Stack and queues/Exercises and Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> reversedInput = new Stack<char>(input.ToCharArray());
+
+            foreach (char ch in reversedInput)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Stack and queues/Exercises and Lab/3. Decimal to Binary Converter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.Decimal_to_Binary_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputDecimal = int.Parse(Console.ReadLine());
+            if (inputDecimal == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            Stack<string> binaryNumber = new Stack<string>();
+
+            while (inputDecimal > 0)
+            {
+                binaryNumber.Push((inputDecimal % 2).ToString());
+
+                inputDecimal /= 2;
+            }
+
+            foreach (var digit in binaryNumber)
+            {
+                Console.Write(digit);
+            }
+        }
+    }
+}
