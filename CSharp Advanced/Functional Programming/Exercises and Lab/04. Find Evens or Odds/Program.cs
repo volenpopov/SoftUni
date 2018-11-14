a753beea19a7d9c013df7bbed0ae173ca2d64436
Volen Popov <volenpopov@abv.bg>
2018-11-14 18:41:40 +0200

    Add files via upload

---
+++ CSharp Advanced/Functional Programming/Exercises and Lab/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = range[0];
+            int end = range[1];
+            string condition = Console.ReadLine();
+            Predicate<int> filter = Valid(condition);
+
+            for (int number = start; number <= end; number++)
+            {
+                if (filter(number))
+                    Console.Write(number + " ");
+            }
+        }       
+        
+        static Predicate<int> Valid(string condition)
+        {
+            if (condition == "even")
+                return num => num % 2 == 0;
+            else
+                return num => num % 2 != 0;
+        }
+    }
+}
---
+++ CSharp Advanced/Functional Programming/Exercises and Lab/3. Count Uppeercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Count_Uppeercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isUpperCase = word => char.IsUpper(word[0]);
+
+            var query = text.Where(word => isUpperCase(word));
+
+            Console.Write(string.Join(Environment.NewLine, query));
+        }
+    }
+}
