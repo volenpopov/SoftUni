1f9ea8e95cf37bfcd9d5e8d4c41907f3b173e9f6
Volen Popov <volenpopov@abv.bg>
2018-11-14 18:05:03 +0200

    Add files via upload

---
+++ CSharp Advanced/Streams/Exercises/1. Read File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _1._Read_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readStream = new StreamReader(@"..\..\..\Program.cs");
+            StreamWriter writeStream = new StreamWriter(@"..\..\..\Text.txt");
+
+            using (readStream)
+            {
+                using (writeStream)
+                {
+                    string textLine = readStream.ReadLine();
+                    int lineNum = 1;
+
+                    while (textLine != null)
+                    {
+                        writeStream.WriteLine(lineNum + " " + textLine);
+                        lineNum++;
+                        textLine = readStream.ReadLine();
+                    }
+                }
+            }
+           
+        }
+    }
+}
