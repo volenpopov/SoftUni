a2c934e34e7a87b49794949f59f16611eb827e39
Volen Popov <volenpopov@abv.bg>
2018-12-29 19:12:13 +0200

    C# ASP.NET Calculator
    C# ASP.NET Calculator

---
+++ Software Technologies/C#/C# ASP.NET MVC Overview- Calculator/Calculator C#/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator.Models.Calculator calculator)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator.Models.Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        public decimal CalculateResult(Calculator.Models.Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+
+                case "/":
+                    if (calculator.RightOperand == 0)
+                    {
+                        break;
+                    }
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+            }
+
+            return result;
+
+        }
+    }
+}
